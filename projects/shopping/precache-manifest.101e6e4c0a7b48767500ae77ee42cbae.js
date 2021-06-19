20c983aa4ed683fccdbe5052e55d30ac90b255e6
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-06-19 18:29:12 -0300

    shopping cart

---
+++ projects/shopping/precache-manifest.101e6e4c0a7b48767500ae77ee42cbae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68701aa36ce2186d48542f9bd1a2f508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2461ee781ea384092fde",
+    "url": "/static/css/main.6f2b81f1.chunk.css"
+  },
+  {
+    "revision": "986bcd750862a890209e",
+    "url": "/static/js/2.e03db5d6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e03db5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2461ee781ea384092fde",
+    "url": "/static/js/main.d35ed9ae.chunk.js"
+  },
+  {
+    "revision": "837674402e425cca2f8d",
+    "url": "/static/js/runtime-main.6839dbe2.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/static/media/search-solid.c6a0effb.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/times-solid.2c5552a1.svg"
+  }
+]);
